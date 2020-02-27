8762d4f6af796b51af629c549ff80e45c73339ee
nanguoyu <admin@nanguoyu.com>
2020-02-27 22:36:22 +0100

    delete useless comments

--- DenoiseFunction.m
+++ DenoiseFunction.m
@@ -1,11 +1,10 @@
 function [outputArg1] = DenoiseFunction(I)
 %DENOISEFUNCTION Denoise Image Function
 %   
- Ibinary= im2bw(I,graythresh(I));
-Ibinaryf = imfilter(Ibinary,fspecial('average',5),'replicate');
- se1 =strel('disk',1);
-IbinaryAfterClosing=imclose(Ibinaryf,se1);
-
-outputArg1 = IbinaryAfterClosing;
+    Ibinary= im2bw(I,graythresh(I));
+    Ibinaryf = imfilter(Ibinary,fspecial('average',5),'replicate');
+    se1 =strel('disk',1);
+    IbinaryAfterClosing=imclose(Ibinaryf,se1);
+    outputArg1 = IbinaryAfterClosing;
 end
 
